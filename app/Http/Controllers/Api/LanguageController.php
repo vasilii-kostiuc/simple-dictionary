0c1904b1d8839084fa93f239fb6829dd4da5f586
vasea <vasile.costiuc@it-lab.md>
2025-02-05 12:34:27 +0200

    api installed  api:install LanguageController

---
+++ app/Http/Controllers/Api/LanguageController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Language;
+use Illuminate\Http\Request;
+
+class LanguageController extends Controller
+{
+    public function index()
+    {
+        return Language::all();
+    }
+}
