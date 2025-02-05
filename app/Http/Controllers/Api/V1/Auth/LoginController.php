86af87b7cfd211c2cd62fd2721687bfbed88574c
vasea <vasile.costiuc@it-lab.md>
2025-02-05 17:42:14 +0200

    Login endpoint implemented Auth tests

---
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginUserRequest;
+use App\Models\User;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+
+    public function __invoke(LoginUserRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+
+        $device = $request->userAgent() ?? '';
+        $expiresAt = $request->remember ? null : now()->addMinutes(config('session.lifetime'));
+
+         return response()->json([
+             'access_token' => $user->createToken($device, expiresAt: $expiresAt)->plainTextToken,
+         ], Response::HTTP_OK);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,18 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')/*->middleware('auth:sanctum')*/->group(
-    function () {
-        Route::post('auth/register', \App\Http\Controllers\Api\V1\Auth\RegisterController::class);
+
+Route::prefix('v1')/*->middleware('auth:sanctum')*/ ->group(function () {
+    Route::post('auth/register', \App\Http\Controllers\Api\V1\Auth\RegisterController::class);
+    Route::post('auth/login', \App\Http\Controllers\Api\V1\Auth\LoginController::class);
+
+    Route::middleware(['auth:sanctum'])->group(function () {
 
         Route::get('languages', [\App\Http\Controllers\Api\V1\LanguageController::class, 'index']);
         Route::get('languages/{language}', [\App\Http\Controllers\Api\V1\LanguageController::class, 'show']);
 
-
         Route::get('/user', function (Request $request) {
             return $request->user();
-        })->middleware('auth:sanctum');
+        });
     });
+});
