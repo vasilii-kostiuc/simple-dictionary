33a39a5bc0dc36cb9185583ad01829d2636ae653
vasea <vasile.costiuc@it-lab.md>
2025-02-17 16:13:03 +0200

    User resource email fix

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -17,7 +17,7 @@ class UserResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'code' => $this->email,
+            'email' => $this->email,
         ];
     }
 }
