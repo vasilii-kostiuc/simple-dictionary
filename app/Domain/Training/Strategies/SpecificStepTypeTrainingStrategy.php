019710118286246ac07c1253f590da5cf869eaee
vasea <vasile.costiuc@it-lab.md>
2025-07-23 18:06:38 +0300

    Refactor `TrainingStepController` to update method signature, adjust `TrainingStepProgressResource` fields, and enhance `SpecificStepTypeTrainingStrategy` constructor for improved step handling and progress tracking.

--- app/Domain/Training/Strategies/SpecificStepTypeTrainingStrategy.php
+++ app/Domain/Training/Strategies/SpecificStepTypeTrainingStrategy.php
@@ -3,15 +3,20 @@
 namespace App\Domain\Training\Strategies;
 
 use App\Domain\Training\Enums\TrainingStepType;
+use App\Domain\Training\Factories\TrainingStepFactory;
+use App\Domain\Training\Models\Training;
 use App\Domain\Training\Steps\WordTrainingStep;
 
 class SpecificStepTypeTrainingStrategy extends TrainingStrategyAbstract
 {
     private TrainingStepType $stepType;
 
-    public function __construct(TrainingStepType $stepType)
+
+    public function __construct(Training $training, TrainingStepFactory $trainingStepFactory, TrainingStepType $stepType)
     {
         $this->stepType = $stepType;
+
+        parent::__construct($training, $trainingStepFactory);
     }
 
     public function generateNextStep(): WordTrainingStep
